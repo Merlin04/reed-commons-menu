8c0e7ce21869387aa6ff7a68d4d17b275a396f09
Merlin04 <Merlin04@users.noreply.github.com>
2023-12-09 16:00:42 -0800

    Template additions

--- bin/template.eml.ml
+++ bin/template.eml.ml
@@ -1,19 +1,47 @@
+open Scraper
+
 let render () =
+  <!DOCTYPE html>
   <html>
   <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/light.css" />-->
+    <link rel="stylesheet" href="/assets/app.css" />
     <title>Reed Commons Menu</title>
 % if !Scraper.data |> Option.is_none then begin
-    <meta http-equiv="refresh" content="1">
+    <meta http-equiv="refresh" content="1" />
 % end
 % else ();
   </head>
   <body>
-    <h1>Yeah!</h1>
+    <h1>Reed Commons Menu</h1>
     this is testing ! another test
 % if !Scraper.data |> Option.is_some then begin
 
-% (!Scraper.data) |> Option.get |> Scraper.dayparts |> List.iter begin fun { Scraper.label; _} ->
-    <h1><%s label %></h1>
+% (!Scraper.data) |> Option.get |> Scraper.dayparts |> List.iter begin fun { label; starttime; endtime; stations; message; _ } ->
+    <h1><%s label %> (open <%s starttime %> to <%s endtime %>)</h1>
+%if message <> "" then begin
+    <p class="alert"><%s message %></p>
+% end
+% else ();
+% stations |> List.iter begin fun { label; items; _ } ->
+    <h2><%s label %></h2>
+    <ul>
+% items |> List.iter begin fun item_id -> let { label; description; price; _ } = (!Scraper.data |> Option.get).items |> List.find (fun ({ id; _ } : menu_item) -> id = item_id) in
+      <li><%s label %>
+% if description <> "" then begin
+        <br /><span class="description"><%s description %></span>
+% end
+% else ();
+% if price <> "" then begin
+        <br /><span class="price"><%s price %></span>
+% end
+% else ();
+      </li>
+% end;
+    </ul>
+% end;
 % end;
 
     <%s !Scraper.data |> Option.get |> Scraper.show %>
--- bin/web.ml
+++ bin/web.ml
@@ -1,6 +1,11 @@
 open Template
 open Args
 
+let loader _root path _request =
+  match Assets.read path with
+    | None -> Dream.empty `Not_Found
+    | Some asset -> Dream.respond asset
+
 let run () =
   Dream.log "Running at http://localhost:%d" args.port;
   Dream.serve ~port:args.port
@@ -10,4 +15,5 @@ let run () =
     Dream.get "/" (fun _request ->
       render () |> Dream.html);
     Dream_livereload.route ();
+    Dream.get "/assets/**" (Dream.static ~loader "")
   ]
